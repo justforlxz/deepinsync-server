dd6a25f8ad24ab5eed681cfd2830ed78d4a23d62
kirigaya <kirigaya@mkacg.com>
2017-05-19 15:07:47 +0800

    build queue

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build ignore
-
 package main
 
 import (
 	"flag"
 	"log"
 	"net/http"
+
+	"fmt"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,30 +14,62 @@ var addr = flag.String("addr", "0.0.0.0:1996", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
+// var s []Queue
+
+var err error
+
+var m map[string][]*websocket.Conn
+
 func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
+	//check
+	c, ok := m[r.URL.Path]
+
+	if ok {
+		c = append(c, conn)
+		fmt.Println("追加指针")
+	} else {
+		var c []*websocket.Conn
+		c = append(c, conn)
+		m[r.URL.Path] = c
+		fmt.Println("创建第一个指针")
+	}
+
+	log.Println(r.URL.Path)
+
+	defer conn.Close()
 	for {
-		mt, message, err := c.ReadMessage()
+		mt, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
 		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
+
+		//分发数据
+		for _, s := range m[r.URL.Path] {
+			if s == conn {
+				fmt.Println("跳过当前建立链接的指针")
+				continue
+			}
+			fmt.Println("正在分发数据")
+			err = s.WriteMessage(mt, message)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
 		}
 	}
 }
 
-
 func main() {
+	m = make(map[string][]*websocket.Conn)
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/", echo)
